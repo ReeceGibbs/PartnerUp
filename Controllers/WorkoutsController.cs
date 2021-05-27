00e8011afdbc071516bfe74fc0dfed65207060b2
reece.gibbs <reece.gibbs@prosol.co.za>
2021-05-27 17:50:01 +1200

    API complete.

--- Controllers/WorkoutsController.cs
+++ Controllers/WorkoutsController.cs
@@ -73,5 +73,77 @@ namespace PartnerUp.Controllers
             //we return an HttpActionResult that reflects that everything went well
             return Ok("Workout Successfully Inserted");
         }
+
+        //we write a handler for an HTTP Put request to update a workout record given an id
+        [HttpPut]
+        public IHttpActionResult UpdateWorkout(Workout workout)
+        {
+
+            //we check to see if the data is valid
+            if (!ModelState.IsValid)
+            {
+
+                //if the workout object passed here is invalid, then we return a BadRequest
+                return BadRequest("Invalid Workout Object");
+            }
+
+            //we begin a using block to reference our DbContext
+            using (PartnerUpEntities entities = new PartnerUpEntities())
+            {
+
+                //we define a new Workout object and assign it the record in the database that has the id passed to this handler
+                Workout dbWorkout = entities.Workouts.FirstOrDefault(w => w.id == workout.id);
+
+                //we check to see if the workout object that the user was looking for existed
+                if (dbWorkout == null)
+                {
+
+                    //if so, we return a BadRequest
+                    return BadRequest("Invalid Record Id");
+                }
+
+                //if the record id was valid, then we want to update the record with the information the user provided
+                dbWorkout.date = workout.date;
+                dbWorkout.exercises = workout.exercises;
+                dbWorkout.reps = workout.reps;
+                dbWorkout.sets = workout.sets;
+
+                //we save the changes to the database
+                entities.SaveChanges();
+            }
+
+            return Ok("Workout Record Successfully Updated");
+        }
+
+        //we write a handler for an HTTP Delete request to delete a workout record given an id
+        [HttpDelete]
+        public IHttpActionResult DeleteWorkout(int id)
+        {
+
+            //we begin a using block to reference our dbcontext
+            using (PartnerUpEntities entities = new PartnerUpEntities())
+            {
+
+                //we define a workout object that is representative of the workout record with the given id
+                Workout workout = entities.Workouts.FirstOrDefault(w => w.id == id);
+
+                //we check to see if the record id is valid
+                if (workout == null)
+                {
+
+                    //if the id is invalid, then we return a BadRequest
+                    return BadRequest("Invalid Record Id");
+                }
+
+                //if the record does exist, then we want to remove it from the database
+                entities.Workouts.Remove(workout);
+
+                //we save the changes to the database
+                entities.SaveChanges();
+            }
+            
+            //if all actions were completed successfully, then we want to return a message that indicates that
+            return Ok("Workout Record Successfully Deleted");
+        }
     }
 }
